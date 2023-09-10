9556ac86c3ca7627ef325fb92137a0193034db98
IconicDexter <kennymwendwa67@gmail.com>
2023-09-10 16:31:55 +0300

    npm i bcrypt @types/bcrypt @auth/prisma-adapter

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,15 +1,4 @@
-import type { NextAuthOptions } from "next-auth";
 import NextAuth from "next-auth/next";
-import GithubProvider from "next-auth/providers/github";
-
-export const authOptions: NextAuthOptions = {
-  providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID ?? "",
-      clientSecret: process.env.GITHUB_SECRET ?? "",
-    }),
-  ],
-};
 
 const handler = NextAuth(authOptions);
 
