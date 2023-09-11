505f8574788ce8a5b4ab77774e37815d4e955181
IconicDexter <kennymwendwa67@gmail.com>
2023-09-11 21:46:30 +0300

    npm i react-hook-form react-icons @hookform/resolvers

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,11 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  darkMode: ["class"],
+import type { Config } from "tailwindcss";
+
+const config: Config = {
   content: [
-    "./pages/**/*.{ts,tsx}",
-    "./components/**/*.{ts,tsx}",
-    "./app/**/*.{ts,tsx}",
-    "./src/**/*.{ts,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  plugins: [],
 };
+export default config;
