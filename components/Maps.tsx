2dbf2865a9a57a968791327ef7974ccb0b3a18c3
IconicDexter <kennymwendwa67@gmail.com>
2023-09-14 12:23:17 +0300

    Changed name to travel-advisor

--- components/Maps.tsx
+++ components/Maps.tsx
@@ -1,9 +1,23 @@
 "use client";
 
+import GoogleMapReact from "google-map-react";
+
 export default function Maps() {
+  const coordinates = { lat: 0, lng: 0 };
   return (
     <>
-      <div> Maps</div>
+      <div className="container mx-auto h-screen">
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: "" }}
+          defaultCenter={coordinates}
+          center={coordinates}
+          defaultZoom={14}
+          margin={[50, 50, 50, 50]}
+          //   options={}
+          //   onChange={}
+          //   onChildClick={}
+        ></GoogleMapReact>
+      </div>
     </>
   );
 }
